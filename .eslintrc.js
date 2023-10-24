f7e1f0c10237a37ff3b0937bfe27db4057efc190
guanwen.fu@versa-ai.com <guanwen.fu@versa-ai.com>
2023-10-24 09:56:57 +0800

    chore: init local env

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // 这里添加一行规则把这条规则隐藏
+    'import/prefer-default-export': 0,
+    'linebreak-style': 0,
+  },
+};
