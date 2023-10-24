b596d99142cf7bf745c6eb816b779b5ad2815a6b
guanwen.fu@versa-ai.com <guanwen.fu@versa-ai.com>
2023-10-24 13:47:57 +0800

    chore: add ci

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,29 +3,16 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    // 这里增加一行，用于支持后面的测试环境
     jest: true,
   },
-  extends: 'airbnb-base',
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
+  extends: ['airbnb-base'],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 13,
     sourceType: 'module',
   },
   rules: {
     // 这里添加一行规则把这条规则隐藏
     'import/prefer-default-export': 0,
-    'linebreak-style': 0,
   },
 };
